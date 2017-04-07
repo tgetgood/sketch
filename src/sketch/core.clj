1d3f0471b1bba06a9394056e134c7415cf019580
Thomas Getgood <thomas.getgood@gmail.com>
2017-04-07 12:13:32 -0400

    More notes.

--- src/sketch/core.clj
+++ src/sketch/core.clj
@@ -1,5 +1,6 @@
 (ns sketch.core
-  (:require [quil.core :as q]))
+  (:require [quil.core :as q]
+            [clojure.core.reducers :as r]))
 
 (defn setup []
   (q/frame-rate 1)
@@ -7,7 +8,6 @@
 
 (defn draw []
   )
-
 
 
 (defmacro defalias [spec aliases]
@@ -20,3 +20,16 @@
 ;; Takes ~100ms for l = 2206948
 
 (def x 348)
+
+(defn rf
+  "Adds up the differing pixels"
+  ([] 0)
+  ([x] x)
+  ([acc [x y]]
+   (if (= x y)
+     acc
+     (inc acc))))
+
+(def l 256)
+
+(def p (into [] (take l (repeatedly (fn [] [(rand-int 255) (rand-int 255)])))))
